711c608e5ab35a250c6b56431373ea11e7c3b5dd
sashanik21 <sashanik21@gmail.com>
2026-06-11 14:23:06 +0300

    Update cards.py

--- app/collectors/cards.py
+++ app/collectors/cards.py
@@ -1,1 +1,28 @@
+from app.config import HEADERS
+from app.wb_client import WBClient
 
+
+def get_cards_list():
+
+    client = WBClient(HEADERS)
+
+    url = "https://content-api.wildberries.ru/content/v2/get/cards/list"
+
+    payload = {
+        "settings": {
+            "cursor": {
+                "limit": 10
+            },
+            "filter": {
+                "withPhoto": -1
+            }
+        }
+    }
+
+    data = client.request(
+        method="POST",
+        url=url,
+        json_data=payload
+    )
+
+    return data
