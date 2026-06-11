9f0cd9c9f660b45201d95063c4ecc82aa6083319
sashanik21 <sashanik21@gmail.com>
2026-06-11 14:15:59 +0300

    Implement WB_API_TOKEN from environment variables
    Add environment variable handling for WB_API_TOKEN

--- app/config.py
+++ app/config.py
@@ -1,1 +1,12 @@
+import os
 
+
+WB_API_TOKEN = os.getenv("WB_API_TOKEN_TEST")
+
+if not WB_API_TOKEN:
+    raise ValueError("WB_API_TOKEN_TEST не найден в GitHub Secrets")
+
+
+HEADERS = {
+    "Authorization": WB_API_TOKEN
+}
